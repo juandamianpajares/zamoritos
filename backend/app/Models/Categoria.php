e1d7bcc12cd29f2dbad22839d508e1e976245857
Juan Damián Pajares <damian.pajares@docente.ceibal.edu.uy>
2026-03-26 03:37:27 -0300

    catalogo

--- backend/app/Models/Categoria.php
+++ backend/app/Models/Categoria.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
@@ -19,5 +20,15 @@ class Categoria extends Model
     public function productos(): HasMany
     {
         return $this->hasMany(Producto::class);
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Categoria::class, 'parent_id');
     }
 }
